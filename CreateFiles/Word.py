69174d93bd8d1d145bfc42a812cb0ef2a1e56b4f
DmitryGrusha <Dima-fx@ukr.net>
2025-02-05 20:10:16 +0200

    [NEW] update dontors in Problems

--- CreateFiles/Word.py
+++ CreateFiles/Word.py
@@ -72,7 +72,6 @@ def createWordA5(fullName: str, sex: str, birthday: str, age: str, job: str, pos
 
 
     specialties = sort_specialties(specialties)
-    print(specialties)
 
     for i, (specialty, cabinet) in enumerate(specialties):
 
